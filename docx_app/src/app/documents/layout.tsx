1bcad4440f87180816112251e956d89859b644a6
yogireddy21 <yogeswarreddyrachamallu@gmail.com>
2025-01-06 17:46:26 +0530

    layout_pagedone

---
+++ docx_app/src/app/documents/layout.tsx
@@ -0,0 +1,13 @@
+interface DocumentsLayoutProps {
+  children: React.ReactNode;
+};
+const DocumentsLayout = ({children}: DocumentsLayoutProps) => {
+    return ( 
+        <div className="flex flex-col gap-y-4">
+            <nav className="w-full bg-red-500">DocumentNavbar</nav>
+           {children}
+        </div>
+     );
+}
+ 
+export default DocumentsLayout;
