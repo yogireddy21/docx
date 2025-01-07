79b6ec13f60f017e1f8501684030f3370021480f
yogireddy21 <yogeswarreddyrachamallu@gmail.com>
2025-01-07 12:29:41 +0530

    tip-tap extesnions done

--- docx_app/src/app/documents/[documentid]/editor.tsx
+++ docx_app/src/app/documents/[documentid]/editor.tsx
@@ -1,5 +1,13 @@
 "use client";
 import {useEditor , EditorContent} from '@tiptap/react'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
 import StarterKit from '@tiptap/starter-kit'
 export const Editor = () =>{
 const editor = useEditor({
@@ -9,8 +17,37 @@ const editor = useEditor({
          class:"focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
       },
     },
-    extensions: [StarterKit],
-    content: '<p>Hello World!</p>',
+    extensions: [
+        StarterKit,
+        TaskItem.configure({
+            nested:true,
+        }),
+        TaskList,
+        Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+      ImageResize,
+    ],
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
 })
    return (
         <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
