ea53406332b656fbcff21f8cfe09c58c3cb00a14
yogireddy21 <yogeswarreddyrachamallu@gmail.com>
2025-01-15 16:52:06 +0530

    minor_error_fixed

--- docx_app/src/components/convex-client-provider.tsx
+++ docx_app/src/components/convex-client-provider.tsx
@@ -3,7 +3,7 @@
 import { ConvexReactClient ,Authenticated,Unauthenticated,AuthLoading} from "convex/react";
 import { ReactNode } from "react";
 import {ConvexProviderWithClerk } from "convex/react-clerk";
-import { ClerkProvider,useAuth ,SignIn} from "@clerk/clerk-react" 
+import { ClerkProvider,useAuth ,SignIn} from "@clerk/nextjs" 
 import { FullscreenLoader } from "./fullscreen-loader";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -16,7 +16,7 @@ export function ConvexClientProvider({ children }: { children: ReactNode }) {
             </Authenticated>
             <Unauthenticated>
                <div className="flex flex-col items-center justify-center min-h-screen">
-                <SignIn/>
+                <SignIn routing="hash"/>
                </div>
             </Unauthenticated>
             <AuthLoading><FullscreenLoader label="Auth Loading...."/></AuthLoading>
